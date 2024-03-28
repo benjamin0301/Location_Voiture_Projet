c757b429bd8389287914e1f63214be1f5974c9a7
antoine3107 <clashinfos2020@gmail.com>
2024-03-28 18:57:03 +0100

    - Antoine :
    Ajout d'une barre de navigation (scrollbar) sur la droite de la fenêtre.
    
    Le conteneur principal a été imaginé différemment : c'est en fait un gros conteneur (fond gris) qui contient tous les éléments de la partie principale du site (la bande rose saumon, le conteneur noir qui est sur la gauche et le conteneur bleu qui contient les bandes bleues). C'est donc le conteneur gris qui imbrique tous les éléments que je viens de citer.
    
    J'ai aussi fait qeulques recherches en parallèle de cela. J'ai vu qu'on peut changer de page sur notre application sans pour autant faire ouvrir une nouvelle fenêtre d'affichage.
    
    Pour la suite, je prévois d'ajouter un carousel d'éléments dans le conteneur bleu et d'affiner la structure globale de cette page, en m'inspirant toujours du site d'exemple donné dans le sujet du projet.

--- src/Controleur.java
+++ src/Controleur.java
@@ -5,5 +5,6 @@ public class Controleur {
     public static void main(String[] s) throws SQLException, ClassNotFoundException {
         // creation de la fenetre
         Connexion fenetre = new Connexion("location_voiture", "root", "");
+
     }
 }
